337e1decb8a0e217bbfeade43b8cb7b78efaa2ff
Dalla Dirosa <dalladirosa@gmail.com>
2018-10-14 13:30:06 +0700

    Remove module

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,27 @@ require('@babel/polyfill')
 module.exports = {
   mode: 'production',
   entry: {
-    index: ['@babel/polyfill', './public/src/index.js']
+    index: './public/src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: ['@babel/preset-env']
+  //         }
+  //       },
+  //       query: require('./.babelrc') // reference to babel rules
+  //     }
+  //   ]
+  // },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     publicPath: '/scripts/'
